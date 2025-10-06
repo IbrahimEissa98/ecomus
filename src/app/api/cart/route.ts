2fe7fa00fe789baca7b4ec58bcfabd01b29801eb
IbrahimEissa98 <ibrahimqutb112@gmail.com>
2025-10-07 00:10:56 +0300

    update 3.4

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -3,12 +3,13 @@ import { GetCartApiResponse } from "@/Types";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const token = await getAccessToken();
   const response: GetCartApiResponse = await fetch(
     "https://ecommerce.routemisr.com/api/v1/cart",
     {
       method: "GET",
       headers: {
-        token: (await getAccessToken()) + "",
+        token: token as string,
       },
     }
   ).then((res) => res.json());
--- src/helpers/getToken.ts
+++ src/helpers/getToken.ts
@@ -3,9 +3,7 @@ import { cookies } from "next/headers";
 // : Promise<string | undefined>
 export async function getAccessToken() {
   const sessionCookies = await cookies();
-  const codedToken = sessionCookies.get(
-    "__Secure-next-auth.session-token"
-  )?.value;
+  const codedToken = sessionCookies.get("next-auth.session-token")?.value;
   const accessToken = await decode({
     token: codedToken,
     secret: process.env.NEXTAUTH_SECRET!,
